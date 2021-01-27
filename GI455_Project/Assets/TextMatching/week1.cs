217fce4386d910a0a963ddbe0f3286811a323174
DESKTOP-C03RL2A\Jorenzo <soravit.kaka@bumail.net>
2021-01-28 01:43:35 +0700

    Send Assignment Week1

---
+++ GI455_Project/Assets/TextMatching/week1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class week1 : MonoBehaviour
+{
+    string[] data = { "Doraemon", "Naruto", "Onepiece", "Dragonball", "Conan", "Pokemon" };
+    public Text textData;
+    public Text textCheck;
+
+    public void CheckData()
+    {
+        for (int i = 0; i <= data.Length-1; i++)
+        {
+            if(textData.text == data[i])
+            {              
+                textCheck.text = textData.text + "<color=green> is found </color> "; 
+                return;
+            }
+            else if(textData.text != data[i])
+            {
+                textCheck.text = textData.text + " <color=red> not found </color>";
+            }
+            
+        }
+    }
+}
